1c8c36f4402fe4888efd1c1e42d346fb9eab2f6f
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2022-08-17 13:27:20 -0300

    finalizado03ArraysLoopsEstilosJavaScripts

--- 05 - JavaScript programando na linguagem da web/00AparecidaNutricao/js/principal.js
+++ 05 - JavaScript programando na linguagem da web/00AparecidaNutricao/js/principal.js
@@ -2,40 +2,57 @@ var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+/*
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes)
+var paciente = pacientes[0]
+console.log(paciente)
+var nomePaciente = paciente.querySelector(".info-nome")
+console.log(nomePaciente.textContent)
+*/
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+for (let index = 0; index < pacientes.length; index++) {
+    var paciente = pacientes[index];
+    console.log(paciente)
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido";
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido")
+    }
+    
+    if(altura <= 0 || altura >= 3){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+        paciente.classList.add("paciente-invalido")
 
-var tdImc = paciente.querySelector(".info-imc");
-
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido");
-    tdImc.textContent = "Peso inválido";
-    pesoEhValido = false;
+    }
+    
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(1);
+    } /* else{
+        tdImc.textContent = "Altura e/ou peso inválidos!";    
+    } */
 }
 
-if(altura <= 0 || altura >= 3){
-    console.log("Altura inválida");
-    tdImc.textContent = "Altura inválida!";
-    alturaEhValida = false;
-}
-
-if (pesoEhValido && alturaEhValida) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-} else{
-    tdImc.textContent = "Altura e/ou peso inválidos!";    
-}
 
 
-console.log(peso);
-console.log(altura);
-console.log(imc);
+
 
